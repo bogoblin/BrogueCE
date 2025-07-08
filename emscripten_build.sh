3ae21f2fc06871cf04926f4012513afef56ea7ce
Bobby McCann <bogoblin@gmail.com>
2025-07-08 20:06:39 +0100

    emscripten build

--- emscripten_build.sh
+++ emscripten_build.sh
@@ -3,3 +3,4 @@
 emmake make bin/brogue.html \
   CFLAGS="--use-port=sdl2 --use-port=sdl2_image:formats=png --embed-file bin/assets -sALLOW_MEMORY_GROWTH -sSTACK_SIZE=1mb -sASYNCIFY" \
   DATADIR="/bin"
+cp bin/brogue.html bin/index.html
