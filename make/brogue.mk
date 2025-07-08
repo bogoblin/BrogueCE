8827b4b8cb2bc3e6fb4ddeb8bcf0c125274389f8
Bobby McCann <bogoblin@gmail.com>
2025-07-08 20:38:48 +0100

    emscripten build

--- make/brogue.mk
+++ make/brogue.mk
@@ -1,2 +1,2 @@
-bin/brogue bin/brogue.exe bin/brogue.html: $(objects) vars/cflags vars/LDFLAGS vars/libs vars/objects make/brogue.mk
+bin/brogue bin/brogue.exe bin/brogue.html: $(objects) vars/cflags vars/LDFLAGS vars/libs vars/objects make/brogue.mk shell_minimal.html
 	$(CC) $(cflags) $(LDFLAGS) -o $@ $(objects) $(libs)
