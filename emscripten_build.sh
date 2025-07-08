b2c88ff32189a4a457eb40dc05d41f4e8bc54417
Robert McCann <robert.mccann@codethink.co.uk>
2025-07-08 19:28:17 +0100

    emscripten build

--- emscripten_build.sh
+++ emscripten_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source emsdk/emsdk_env.sh
+#source ../emsdk/emsdk_env.sh
 emmake make bin/brogue.html \
   CFLAGS="--use-port=sdl2 --use-port=sdl2_image:formats=png --embed-file bin/assets -sALLOW_MEMORY_GROWTH -sSTACK_SIZE=1mb -sASYNCIFY" \
   DATADIR="/bin"
