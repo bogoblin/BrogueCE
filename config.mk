5f57b400ca1c32039fafc7e3ec5f5cf667452620
Bobby McCann <bogoblin@gmail.com>
2025-07-10 15:54:40 +0100

    Use idbfs

--- config.mk
+++ config.mk
@@ -16,7 +16,7 @@ SDL_CONFIG := sdl2-config
 WEBBROGUE := NO
 
 # Enable debugging mode. See top of Rogue.h for features
-DEBUG := NO
+DEBUG := YES
 
 # Declare this is a release build
 RELEASE := NO
--- emscripten_build.sh
+++ emscripten_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #source ../emsdk/emsdk_env.sh
 emmake make -B bin/brogue.html \
-  CFLAGS="--use-port=sdl2 --use-port=sdl2_image:formats=png --embed-file bin/assets -sALLOW_MEMORY_GROWTH -sSTACK_SIZE=1mb -sASYNCIFY --shell-file shell_minimal.html" \
+  CFLAGS="--use-port=sdl2 --use-port=sdl2_image:formats=png --embed-file bin/assets -sALLOW_MEMORY_GROWTH -sSTACK_SIZE=1mb -sASYNCIFY --shell-file shell_minimal.html -lidbfs.js" \
   DATADIR="/bin"
 cp bin/brogue.html bin/index.html
